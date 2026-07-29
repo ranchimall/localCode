6ba9a0b854edd0c64c9d426b2b9eb45a6261a503
SaketAnand <saket2k7@gmail.com>
2026-07-29 12:03:25 +0530

    Revert "adding independent scripts"
    This reverts commit 3b1b6d17b0c14d41467de2947d5a4af60061e9b4.

--- critic/sample.js
+++
@@ -1,9 +0,0 @@
-function calculatePayout(pool, shares) {
-    let total = 0;
-    for (let i = 0; i <= shares.length; i++) {
-        total += pool * shares[i];
-    }
-    return total;
-}
-
-module.exports = { calculatePayout };
